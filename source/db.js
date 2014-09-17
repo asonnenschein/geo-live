8efa17c805cf00d16e25e491b97a90548b7c5b5b
asonnenschein <adrian.sonnenschein@gmail.com>
2014-09-16 22:51:50 -0700

    js-ify geomonitor pg schema

--- source/db.js
+++ source/db.js
@@ -21,10 +21,54 @@ orm.connect(connectPG, function (err, db) {
     }
   });
 
-  sync.defineCollection('adrians_test', {
-    id: {type: 'serial', key: true, serial: true},
+  sync.defineCollection('hosts', {
     name: {type: 'text', required: true},
-    age: {type: 'integer'}
+    description: {type: 'text', required: true},
+    url: {type: 'text', required: true},
+    institution_id: {type: 'integer', required: true},
+    layers_count: {type: 'integer', required: true},
+    pings_count: {type: 'integer', required: true},
+    created_at: {type: 'date', time: true, required: true},
+    updated_at: {type: 'date', time: true, required: true}
+  });
+
+  sync.defineCollection('institutions', {
+    name: {type: 'text', required: true},
+    created_at: {type: 'date', time: true, required: true},
+    updated_at: {type: 'date', time: true, required: true}
+  });
+
+  sync.defineCollection('layers', {
+    name: {type: 'text', required: true},
+    geoserver_layername: {type: 'text', required: true},
+    access: {type: 'text', required: true},
+    description: {type: 'text', required: true},
+    bbox: {type: 'text', required: true},
+    host_id: {type: 'integer', required: true},
+    statuses_count: {type: 'integer', required: true},
+    created_at: {type: 'date', time: true, required: true},
+    updated_at: {type: 'date', time: true, required: true}
+  });
+
+  sync.defineCollection('pings', {
+    status: {type: 'boolean', required: true},
+    latest: {type: 'boolean', required: true},
+    host_id: {type: 'integer', required: true},
+    created_at: {type: 'date', time: true, required: true},
+    updated_at: {type: 'date', time: true, required: true}
+  });
+
+  sync.defineCollection('statuses', {
+    res_code: {type: 'text', required: true},
+    res_message: {type: 'text', required: true},
+    res_time: {type: 'number', size: 8, required: true},
+    status: {type: 'text', required: true},
+    status_message: {type: 'text', required: true},
+    submitted_query: {type: 'text', required: true},
+    latest: {type: 'boolean', required: true},
+    layer_id: {type: 'integer', required: true},
+    created_at: {type: 'date', time: true, required: true},
+    updated_at: {type: 'date', time: true, required: true}
   });
 
   sync.sync(function (err) {
